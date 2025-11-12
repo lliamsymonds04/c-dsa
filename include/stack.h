0b01b8772b4e621ec9484b95f11bcc5e00a55dda
Lliam Symonds <lliamsymonds04@gmail.com>
2025-11-12 18:11:59 +1000

    added stack.h

---
+++ include/stack.h
@@ -0,0 +1,17 @@
+#ifndef STACK_H
+#define STACK_H
+
+#include <stdlib.h>
+#include "dynarray.h"
+
+typedef struct {
+  DynArray *array;
+} Stack;
+
+Stack *stack_create(size_t element_size);
+void stack_free(Stack *stack);
+int stack_push(Stack *stack, const void *element);
+int stack_pop(Stack *stack);
+void *stack_top(const Stack *stack);
+size_t stack_size(const Stack *stack);
+int stack_is_empty(const Stack *stack);

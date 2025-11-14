db2ea0b953b52eda8901658baca4675f692efec4
Lliam Symonds <lliamsymonds04@gmail.com>
2025-11-14 17:37:20 +1000

    added linked list tests

--- src/main.c
+++ src/main.c
@@ -1,20 +1,33 @@
 #include <stdio.h>
 #include <string.h>
 
-void test_dynarray(void);
-void test_stack(void);
+int test_dynarray(void);
+int test_stack(void);
+int test_linked_list(void);
 
 int main(int argc, char *argv[]) {
   int run_all = (argc == 1);
   const char *test_name = run_all ? NULL : argv[1];
 
+  int passed = 0;
+  int total_tests = 0;
+
   if (run_all || strcmp(test_name, "dynarray") == 0) {
-    test_dynarray();
+    passed += test_dynarray();
+    total_tests++;
   }
 
   if (run_all || strcmp(test_name, "stack") == 0) {
-    test_stack();
+    passed += test_stack();
+    total_tests++;
   }
+
+  if (run_all || strcmp(test_name, "linked_list") == 0) {
+    passed += test_linked_list();
+    total_tests++;
+  }
+
+  printf("\n [OVERALL] Passed %d out of %d tests.\n", passed, total_tests);
 
   return 0;
 }
--- tests/test_linked_list.c
+++ tests/test_linked_list.c
@@ -1,1 +1,50 @@
 #include "linked_list.h"
+#include <stdio.h>
+
+void output_linked_list(LinkedList *list) {
+  Node *current = list->head;
+  printf("LinkedList (size=%zu): ", list->size);
+  while (current) {
+    printf("%d -> ", *(int *)current->value);
+    current = current->next;
+  }
+  printf("NULL\n");
+}
+
+int test_linked_list(void) {
+  printf("[TEST] linked_list...\n");
+  LinkedList *list = linked_list_create(sizeof(int));
+
+  // Test added at tail
+  for (int i = 1; i <= 5; i++) {
+    add_at_tail(list, &i);
+  }
+
+  add_at_head(list, &(int){0});
+
+  output_linked_list(list);
+
+  // remove tail
+  remove_at_tail(list);
+
+  output_linked_list(list);
+
+  // Reverse list
+  linked_list_reverse(list);
+
+  output_linked_list(list);
+
+  printf("Find node with value 3: ");
+  Node *node_3 = find_node(list, &(int){3});
+  if (node_3) {
+    printf("Found node with value %d\n", *(int *)node_3->value);
+  } else {
+    printf("Node not found\n");
+  }
+
+  printf("Passed all linked list tests.\n");
+
+  linked_list_free(list);
+
+  return 1;
+}
